7cee8acee44e8a65d60d9792772fcc63374e47c4
Dhanush.S <132473127+Dhxnushh@users.noreply.github.com>
2024-02-07 10:29:23 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ function no(){
     for(let i=1;i<=subj;i++){
         content = `
         <h3>Subject ${i}:</h3>
-        <input type="number" id="${n}" placeholder="Marks">
-        <input type="number" id='${n+1}' placeholder="Credits">
+        <input type="number" id="m${n}" placeholder="Marks">
+        <input type="number" id='c${n+1}' placeholder="Credits">
         `
         n+=2
         content1.innerHTML+=content;
     }
-    n=0;
+    n=subj+2;
     for(let i=1;i<=lab;i++){
         content = `
         <h3>Lab ${i}:</h3>
-        <input type="number" id="${n}" placeholder="Marks">
-        <input type="number" id='${n+1}' placeholder="Credits">
+        <input type="number" id="m${n}" placeholder="Marks">
+        <input type="number" id='c${n+1}' placeholder="Credits">
         `
         n+=2
         content1.innerHTML+=content;
@@ -28,60 +28,50 @@ function no(){
     content1.innerHTML+=`<br>
                          <br>
                          <button id='lol' onclick='calc(${subj},${lab})'>Calculate<button>`;
-    
-    const grade = [] ;
-    for (let i=0;i<6;i++){
-        if (l[i]>=90){
-            grade[i]=10;
-        }
-        else if(l[i]>=80){
-            grade[i] = 9;
-        }
-        else if(l[i]>=70){
-            grade[i] = 8;
-        }
-        else if(l[i]>=60){
-            grade[i] = 7;
-        }
-        else if(l[i]>=50){
-            grade[i] = 6;
-        }
-        else if(l[i]<50){
-            grade[i] = 0;
-        }
-    }
-    console.log(grade);
-    const credits = [c1,c2,c3,c4,c5,c6];
-    const credit = c1+c2+c3+c4+c5+c6;
-    let total_points = 0;
-    for(let j=0;j<6;j++){
-        total_points+=grade[j]*credits[j];
-    }
-
-    gpa = (total_points/credit);
-    document.getElementById("gpa").textContent = `Your GPA IS: ${gpa}`  
 }
 
-function add(){
-    var input = document.createElement("input");
-    input.setAttribute("type","text")
-    input.setAttribute("placeholder","Marks")
-    document.getElementById('new').innerHTML+=input;``
-    return input.id;
-}
-
-function reset(){
-    newfield = document.getElementById("new");
-    newfield.innerHTML="";
-}
 
 function calc(subj,lab){
-    var grade = [];
+    var marks = [];
     var creds = [];
     let x=0;
-    for(let i=0;i<=subj;i++){
-        let mark = document.getElementById('${x}');
-        grade.push(mark);
-        let credits = document.getElementById('${x+1}')
+    for(let j=0;j<=subj;j++){
+        console.log(`${x}`)
+        let mark = Number(document.getElementById(`m${x}`).value);
+        marks[j]=mark;
+        let credits = Number(document.getElementById(`c${x+1}`).value);
+        creds[j]=credits;
+        x+=2
+    }
+    x=subj+2;
+    for(let i=0;i<=lab;i++){
+        let mark = document.getElementById(`m${x}`).value;
+        marks[x]=mark;
+        let credits = document.getElementById(`c${x+1}`).value;
+        creds[x]=mark;
+        x+=2;
+    }
+    console.log(marks);
+    console.log(creds);
+    var grade=[];
+    for (let i=0;i<6;i++){
+        if (marks[i]>=90){
+            grade[i]=10;
+        }
+        else if(marks[i]>=80){
+            grade[i] = 9;
+        }
+        else if(marks[i]>=70){
+            grade[i] = 8;
+        }
+        else if(marks[i]>=60){
+            grade[i] = 7;
+        }
+        else if(marks[i]>=50){
+            grade[i] = 6;
+        }
+        else if(marks[i]<50){
+            grade[i] = 0;
+        }
     }
 }
